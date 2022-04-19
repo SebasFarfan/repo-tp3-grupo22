c19017333ea6baf66e36bf149ff2460725b69c63
Sebas <sebas.654@gmail.com>
2022-04-19 19:50:20 -0300

    generó el Proyecto

---
+++ tp3_grupo22/src/main/java/ar/edu/unju/fi/tp3_grupo22/Tp3Grupo22Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.tp3_grupo22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp3Grupo22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp3Grupo22Application.class, args);
+	}
+
+}
---
+++ tp3_grupo22/src/test/java/ar/edu/unju/fi/tp3_grupo22/Tp3Grupo22ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.tp3_grupo22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp3Grupo22ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
